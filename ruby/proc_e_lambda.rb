035c464e7c5d09d0a14264d96ff692825194f49d
Marcio de Jesus <marciodejesusrj@gmail.com>
2020-01-31 13:40:09 -0300

    Create proc_e_lambda.rb

---
+++ ruby/proc_e_lambda.rb
@@ -0,0 +1,35 @@
+# Lambda = força receber um parametro se for passado um
+# proc = receber um parametro é sempre opcional
+
+```ruby
+l = lambda do |param|
+  puts param
+end
+```
+
+# ```ruby
+# => #<Proc:0x000055ebbdefeaf8@(repl):1 (lambda)>
+#    l.call
+# wrong number of arguments (given 0, expected 1)
+# (repl):1:in `block in <main>'
+# (repl):1:in `<main>'
+#    l.call('bla')
+# bla
+# ```
+
+# proc = Proc.new
+```ruby
+p = proc do |param|
+  puts param
+end
+```
+
+# ```ruby
+# => #<Proc:0x0000563d718eec48@(repl):16>
+#    p.call('aaa')
+# aaa
+# => nil
+#    p.call
+
+# => nil
+# ``` 
