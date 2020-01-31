d38bfd89e874f7a9ccbe21ec2e0e2524ea953b52
Marcio de Jesus <marciodejesusrj@gmail.com>
2020-01-31 13:50:25 -0300

    Create block.rb

---
+++ ruby/block.rb
@@ -0,0 +1,27 @@
+# metodos podem receber blocos e esses blocos podem ser blocos de codigo ou metodos que podemos executar usando por exemplo isntance_eval dentro do metodo
+```ruby
+class ClasseBloco
+  def metodo1
+    puts 'metodo1'
+  end
+  
+  def metodo2
+    puts 'metodo2'
+  end
+  
+  def metodo_que_aceita_bloco(&bloco)
+    instance_eval(&bloco)
+  end
+end
+```
+
+# ```ruby
+#    teste = Teste.new 
+# .. teste.metodo_que_aceita_bloco do 
+# ..   metodo1 
+# ..   metodo2 
+# .. end
+# metodo1
+# metodo2
+# => nil
+# ```
