b5423b6dc8f07ecec5dad85077a45e46d9655764
Marcio de Jesus <marciodejesusrj@gmail.com>
2020-04-13 14:12:44 -0300

    Update base64.rb

--- ruby/base64.rb
+++ ruby/base64.rb
@@ -8,3 +8,21 @@ encoded_string = Base64.encode64(File.open("file_path", "rb").read)
 File.open(file_name_to_create, "wb") do |file|
     file.write(encoded_string)
 end
+
+
+------------------------------------------------
+
+# Fazer conversão em lote
+
+require 'base64'
+
+folder_dir = "spec/fixtures/revenue_upload/"
+files = Dir.entries(folder_dir).select { |f| !File.directory? f} 
+
+files.each do |file_xlsx|
+  encoded_string = Base64.encode64(File.open("#{folder_dir}#{file_xlsx}", "rb").read)
+
+  File.open("#{folder_dir}#{file_xlsx}.base64", "wb") do |file|
+      file.write(encoded_string)
+  end
+end
