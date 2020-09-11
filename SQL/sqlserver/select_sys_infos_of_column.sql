70e87ca529e036bd5f594c61c440210dcb1e9b27
Marcio de Jesus <marciodejesusrj@gmail.com>
2020-09-11 13:47:22 -0300

    Create select_sys_infos_of_column.sql

---
+++ SQL/sqlserver/select_sys_infos_of_column.sql
@@ -0,0 +1,6 @@
+  select * from sys.columns where name='commissionable_value' 
+
+/*
+object_id	name	column_id	system_type_id	user_type_id	max_length	precision	scale	collation_name	is_nullable	is_ansi_padded	is_rowguidcol	is_identity	is_computed	is_filestream	is_replicated	is_non_sql_subscribed	is_merge_published	is_dts_replicated	is_xml_document	xml_collection_id	default_object_id	rule_object_id	is_sparse	is_column_set	generated_always_type	generated_always_type_desc	encryption_type	encryption_type_desc	encryption_algorithm_name	column_encryption_key_id	column_encryption_key_database_name	is_hidden	is_masked
+866102126	commissionable_value	20	106	106	17	32	8	NULL	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	NOT_APPLICABLE	NULL	NULL	NULL	NULL	NULL	0	0
+*/
