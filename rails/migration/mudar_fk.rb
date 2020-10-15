d7ca394e1294f509b0d3c37390a4484901221af2
Marcio de Jesus <marciodejesusrj@gmail.com>
2020-10-15 18:00:35 -0300

    Create mudar_fk.rb

---
+++ rails/migration/mudar_fk.rb
@@ -0,0 +1,27 @@
+# Altera a FK e mantem o sequece ativo
+class CreateAdvisor < ActiveRecord::Migration[6.0]
+  def change
+    create_table :advisor, id: :integer, primary_key: :code do |t|
+      t.string :name
+    end
+  end
+end
+
+# schema.rb
+create_table "advisor", primary_key: "code", id: :integer, force: :cascade do |t|
+  t.string "name"
+end
+
+# Altera a FK e depende do envio do code
+class CreateAdvisor < ActiveRecord::Migration[6.0]
+  def change
+    create_table :advisor, id: :integer, primary_key: :code, default: nil do |t|
+      t.string :name
+    end
+  end
+end
+
+# schema.rb
+create_table "advisor", primary_key: "code", id: :integer, default: nil, force: :cascade do |t|
+  t.string "name"
+end
