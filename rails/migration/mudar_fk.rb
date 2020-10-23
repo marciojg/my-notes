501fc78235b2fa9aaaa7de5af9250de6d0772202
Marcio de Jesus <marciodejesusrj@gmail.com>
2020-10-23 16:48:52 -0300

    Update mudar_fk.rb

--- rails/migration/mudar_fk.rb
+++ rails/migration/mudar_fk.rb
@@ -25,3 +25,20 @@ end
 create_table "client", primary_key: "code", id: :integer, default: nil, force: :cascade do |t|
   t.string "name"
 end
+
+
+# Altera a FK e depende do envio do code mas nao cria um relacionamento com outra tabela
+create_table :client, id: false do |t|
+  t.integer :code, index: { unique: true }
+  t.string :name
+  t.timestamps
+end
+
+# schema.rb
+create_table "client", id: false, force: :cascade do |t|
+  t.integer "code"
+  t.string "name"
+  t.datetime "created_at", precision: 6, null: false
+  t.datetime "updated_at", precision: 6, null: false
+  t.index ["code"], name: "index_advisor_on_code", unique: true
+end
